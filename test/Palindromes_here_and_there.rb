5f08c158032eb6c98661c904dbb7833279f7a824
John Tsai <darkfeathers_0916@hotmail.com>
2019-01-23 15:51:35 +0800

    change from numbera to number_string

--- test/Palindromes_here_and_there.rb
+++ test/Palindromes_here_and_there.rb
@@ -3,15 +3,14 @@
      是否為回文形式
 想法:用each do來對陣列內每個元素來做處理
 後記:用reverse來處理就好啦!!!!
-
 =end
 def convert_palindromes(numbers)
   index_ans = 0
   ans = Array.new(numbers.size, 1)
   numbers.each do |number|
-    numbera = number.to_s
-    0.upto(0.5 * numbera.size) do |index|
-      ans[index_ans] = 0 if numbera[index] != numbera[(-index) - 1]
+    number_string = number.to_s
+    0.upto(0.5 * number_string.size) do |index|
+      ans[index_ans] = 0 if number_string[index] != number_string[(-index) - 1]
     end
     index_ans += 1
   end
