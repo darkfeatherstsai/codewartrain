586d8132d97080911e5987f53240db2049022627
John Tsai <darkfeathers_0916@hotmail.com>
2019-01-30 15:18:16 +0800

    tdd fine

--- test/hop_across.rb
+++ test/hop_across.rb
@@ -1,36 +1,41 @@
 class HopAcross
-  def initialize(place, ans)
-    @place = place
-    @ans = ans
+  def initialize(lst)
+    @lst = lst
   end
 
-  def jump_l_to_r(lst)
+  def hop_across
+    place = 0
+    ans = 0
+    lst = @lst
     until place >= lst.size
       place += lst[place]
       ans += 1
     end
-  end
-
-  def jump_r_to_l(lst)
+    place = -1
     until place < -lst.size
       place -= lst[place]
       ans +=1
     end
+    return ans
   end
-end
 =begin
-def hop_across(lst)
-  place = 0
-  ans = 0
+def initialize(place, ans)
+  @place = place
+  @ans = ans
+end
+
+def jump_l_to_r(lst)
   until place >= lst.size
     place += lst[place]
     ans += 1
   end
-  place = -1
+end
+
+def jump_r_to_l(lst)
   until place < -lst.size
     place -= lst[place]
     ans +=1
   end
-  return ans
 end
 =end
+end
--- testcode/hop_across_spec.rb
+++ testcode/hop_across_spec.rb
@@ -3,8 +3,9 @@ require_relative "../test/hop_across.rb"
 RSpec.describe HopAcross do
   describe "檢驗 Hop Across" do
     it "輸入 [2, 2, 3, 1, 1, 2, 1] 輸出 7" do
-      lst = HopAcross.new([2, 2, 3, 1, 1, 2, 1])
-      expect(result).to eq 7
+      road = HopAcross.new([2, 2, 3, 1, 1, 2, 1])
+      ans = road.hop_across
+      expect(ans).to eq 7
     end
   end
 end
