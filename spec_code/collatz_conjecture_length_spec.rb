82290703ce102e259d627ab6d5d6c59107498fc4
John Tsai <darkfeathers_0916@hotmail.com>
2019-02-01 14:02:17 +0800

    fix

--- spec_code/collatz_conjecture_length_spec.rb
+++ spec_code/collatz_conjecture_length_spec.rb
@@ -1,16 +1,13 @@
 require_relative "../train_code/collatz_conjecture_length.rb"
 
-RSpec.describe CollatzConjectureLength do
+
   describe "檢驗 collatz_conjecture_length" do
     it "輸入 11 輸出 15" do
-      init_number = CollatzConjectureLength.new(11)
-      arr_length = init_number.collatz_conjecture_length
+      arr_length = collatz_conjecture_length(11)
       expect(arr_length).to eq 15
     end
     it "輸入 14 輸出 18" do
-      init_number = CollatzConjectureLength.new(14)
-      arr_length = init_number.collatz_conjecture_length
+      arr_length = collatz_conjecture_length(14)
       expect(arr_length).to eq 18
     end
   end
-end
--- train_code/collatz_conjecture_length.rb
+++ train_code/collatz_conjecture_length.rb
@@ -5,21 +5,20 @@
 想法:依照題目的條件執行，執行完的數字丟入一個新陣列
      最後計算陣列長度
 =end
-class CollatzConjectureLength
-  def initialize(number)
-    @number = number
-  end
+#class CollatzConjectureLength
+  #def initialize(number)
+    #number = number
+  #end
 
-  def collatz_conjecture_length
-    arr = [@number]
-    until @number == 1
-      if @number.odd?
-        @number = @number * 3 + 1
+  def collatz_conjecture_length(number)
+    arr = [number]
+    until number == 1
+      if number.odd?
+        number = number * 3 + 1
       else
-        @number /= 2
+        number /= 2
       end
-      arr << @number
+      arr << number
     end
     return arr.size
   end
-end
